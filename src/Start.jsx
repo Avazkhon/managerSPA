48710880dc6ea9f8617a79f027df115e428219e9
Avazkhon <Kamalxanovavazxon@gmail.come>
2019-03-20 08:48:29 +0700

    Branches component fromUser

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+
+//this state
 import Nav from './Nav';
+import FormUser from './componentStart/formUser'
 
 class Start extends React.Component {
 
@@ -59,49 +62,9 @@ class Start extends React.Component {
        <header>
         <Nav />
       </header>
-  	  <form className="newUser">
-  	  <div className="form-group row">
-	    <label htmlFor="inputFirstNamel3" className="col-sm-2 col-form-label">Имя</label>
-	    <div className="col-sm-10">
-	      <input name="firstName" className="form-control" id="inputFirstNamel3" placeholder="Ввидите имя" onChange={this.handleChange}  />
-	    </div>
-	  </div>
-	  <div className="form-group row">
-	    <label htmlFor="inputLastNamel3" className="col-sm-2 col-form-label">Фамилия</label>
-	    <div className="col-sm-10">
-	      <input name="lastName" className="form-control" id="inputLastNamel3" placeholder="Ввидите фамилию" onChange={this.handleChange} />
-	    </div>
-	  </div>
-	  <div className="form-group row">
-	    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
-	    <div className="col-sm-10">
-	      <input name="email" className="form-control" id="inputEmail3" placeholder="Ввидите email" onChange={this.handleChange} />
-	    </div>
-	  </div>
-	  <div className="form-group row">
-	    <label htmlFor="inputastNickNamel3" className="col-sm-2 col-form-label">Ник</label>
-	    <div className="col-sm-10">
-	      <input name="nickName" className="form-control" id="inputastNickNamel3" placeholder="Ввидите ник" onChange={this.handleChange} />
-	    </div>
-	  </div>
-	  <div className="form-group row">
-	    <label htmlFor="inputPhone3" className="col-sm-2 col-form-label">Телефон</label>
-	    <div className="col-sm-10">
-	      <input name="phone" className="form-control" id="inputPhone3" placeholder="Ввидите телефон" onChange={this.handleChange} />
-	    </div>
-	  </div>
-	  <div className="form-group row">
-	    <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-	    <div className="col-sm-10">
-	      <input name="password" type="password" className="form-control" id="inputPassword3" placeholder="Password" onChange={this.handleChange} />
-	    </div>
-	  </div>
-	  <div className="form-group row">
-	    <div className="col-sm-10">
-	      <input  type='button' className="btn btn-primary" value="зарегистрироваться" onClick={this.handleSendingUser} / >
-	    </div>
-	  </div>
-	</form>
+  	  <FormUser 
+  	    handleChange={this.handleChange} handleSendingUser={this.handleSendingUser}
+  	  />
 	</div>
   	)
   }
---
+++ src/componentStart/formUser.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class FormUser extends React.Component {
+	render() {
+    return (
+    <div className='start'>
+    <form className="newUser">
+    <div className="form-group row">
+      <label htmlFor="inputFirstNamel3" className="col-sm-2 col-form-label">Имя</label>
+    <div className="col-sm-10">
+      <input name="firstName" className="form-control" id="inputFirstNamel3" placeholder="Ввидите имя" onChange={this.props.handleChange}  />
+    </div>
+  </div>
+  <div className="form-group row">
+    <label htmlFor="inputLastNamel3" className="col-sm-2 col-form-label">Фамилия</label>
+    <div className="col-sm-10">
+      <input name="lastName" className="form-control" id="inputLastNamel3" placeholder="Ввидите фамилию" onChange={this.props.handleChange} />
+    </div>
+  </div>
+  <div className="form-group row">
+    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+    <div className="col-sm-10">
+      <input name="email" className="form-control" id="inputEmail3" placeholder="Ввидите email" onChange={this.props.handleChange} />
+    </div>
+  </div>
+  <div className="form-group row">
+    <label htmlFor="inputastNickNamel3" className="col-sm-2 col-form-label">Ник</label>
+    <div className="col-sm-10">
+      <input name="nickName" className="form-control" id="inputastNickNamel3" placeholder="Ввидите ник" onChange={this.props.handleChange} />
+    </div>
+  </div>
+  <div className="form-group row">
+    <label htmlFor="inputPhone3" className="col-sm-2 col-form-label">Телефон</label>
+    <div className="col-sm-10">
+      <input name="phone" className="form-control" id="inputPhone3" placeholder="Ввидите телефон" onChange={this.props.handleChange} />
+    </div>
+  </div>
+  <div className="form-group row">
+    <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+    <div className="col-sm-10">
+      <input name="password" type="password" className="form-control" id="inputPassword3" placeholder="Password" onChange={this.props.handleChange} />
+    </div>
+  </div>
+  <div className="form-group row">
+    <div className="col-sm-10">
+      <input  type='button' className="btn btn-primary" value="зарегистрироваться" onClick={this.props.handleSendingUser} / >
+    </div>
+  </div>
+</form>
+</div>
+  )
+  }
+}
+
+export default FormUser
