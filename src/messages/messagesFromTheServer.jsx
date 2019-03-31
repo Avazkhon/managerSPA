22c3a576a97bb5888680d3712b9648db0c40ef64
Avazkhon <Kamalxanovavazxon@gmail.come>
2019-03-31 08:45:08 +0700

    Add text message

--- src/messages/messagesFromTheServer.jsx
+++ src/messages/messagesFromTheServer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 export default function messang (props, colback) {
   return (
     <div className="jumbotron">
-	  <h1 className="display-4">props.h1</h1>
-	  <p className="lead">props.bigText</p>
+	  <h1 className="display-4">Регистрация компании</h1>
+	  <p className="lead">Вы успешно зарегистрировали копанию. По мере необходимости вы сможете ее дополнить информацией или изменить еe</p>
 	  <hr className="my-4" />
-	  <p>props.smallЕext</p>
+	  <p>За дополнительной информацией вы можете обратиться к документаций</p>
 	  <p className="lead">
 	    <a className="btn btn-primary btn-lg" role="button" onClick={colback} >Ok</a>
 	  </p>
